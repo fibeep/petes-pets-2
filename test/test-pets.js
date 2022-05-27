4477f0b35b0ecbf26a2c645eb2515b28662866f0
Salo <salomonco@gmail.com>
2022-05-27 17:45:17 -0500

    added  basic search test

--- test/test-pets.js
+++ test/test-pets.js
@@ -17,103 +17,121 @@ const fido =     {
 chai.use(chaiHttp);
 
 describe('Pets', ()  => {
-
-  after(() => { 
-    Pet.deleteMany({$or: [{name: 'Norman'}, {name: 'Spider'}] }).exec((err, pets) => {
-      console.log(pets, `Deleted ${pets.n} documents`)
-    }) 
+  after(() => {
+    Pet.deleteMany({ $or: [{ name: "Norman" }, { name: "Spider" }] }).exec(
+      (err, pets) => {
+        console.log(pets, `Deleted ${pets.n} documents`);
+      }
+    );
   });
 
   // TEST INDEX
-  it('should index ALL pets on / GET', (done) => {
-    chai.request(server)
-        .get('/')
+  it("should index ALL pets on / GET", (done) => {
+    chai
+      .request(server)
+      .get("/")
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+      });
+  });
+
+  // TEST NEW
+  it("should display new form on /pets/new GET", (done) => {
+    chai
+      .request(server)
+      .get(`/pets/new`)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+      });
+  });
+
+  // TEST CREATE
+  it("should create a SINGLE pet on /pets POST", (done) => {
+    chai
+      .request(server)
+      .post("/pets")
+      .send(fido)
+      .end((err, res) => {
+        res.should.have.status(200);
+        res.should.be.html;
+        done();
+      });
+  });
+
+  // TEST SHOW
+  it("should show a SINGLE pet on /pets/<id> GET", (done) => {
+    var pet = new Pet(fido);
+    pet.save((err, data) => {
+      chai
+        .request(server)
+        .get(`/pets/${data._id}`)
         .end((err, res) => {
           res.should.have.status(200);
           res.should.be.html;
           done();
         });
-  });
-
-  // TEST NEW
-  it('should display new form on /pets/new GET', (done) => {
-    chai.request(server)
-      .get(`/pets/new`)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.should.be.html
-          done();
-        });
-  });
-  
-  // TEST CREATE 
-  it('should create a SINGLE pet on /pets POST', (done) => {
-    chai.request(server)
-        .post('/pets')
-        .send(fido)
-        .end((err, res) => {
-          res.should.have.status(200);
-          res.should.be.html
-          done();
-        });
-  });
-
-  // TEST SHOW
-  it('should show a SINGLE pet on /pets/<id> GET', (done) => {
-    var pet = new Pet(fido);
-     pet.save((err, data) => {
-       chai.request(server)
-         .get(`/pets/${data._id}`)
-         .end((err, res) => {
-           res.should.have.status(200);
-           res.should.be.html
-           done();
-         });
-     });
-
-  });
-
-  // TEST EDIT
-  it('should edit a SINGLE pet on /pets/<id>/edit GET', (done) => {
-    var pet = new Pet(fido);
-     pet.save((err, data) => {
-       chai.request(server)
-         .get(`/pets/${data._id}/edit`)
-         .end((err, res) => {
-           res.should.have.status(200);
-           res.should.be.html
-           done();
-         });
-     });
-  });
-
-
-  // TEST UPDATE
-  it('should update a SINGLE pet on /pets/<id> PUT', (done) => {
-    var pet = new Pet(fido);
-    pet.save((err, data)  => {
-     chai.request(server)
-      .put(`/pets/${data._id}?_method=PUT`)
-      .send({'name': 'Spider'})
-      .end((err, res) => {
-        res.should.have.status(200);
-        res.should.be.html
-        done();
-      });
     });
   });
 
-  // TEST DELETE
-  it('should delete a SINGLE pet on /pets/<id> DELETE', (done) => {
+  // TEST EDIT
+  it("should edit a SINGLE pet on /pets/<id>/edit GET", (done) => {
     var pet = new Pet(fido);
-    pet.save((err, data)  => {
-     chai.request(server)
-      .delete(`/pets/${data._id}?_method=DELETE`)
+    pet.save((err, data) => {
+      chai
+        .request(server)
+        .get(`/pets/${data._id}/edit`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.html;
+          done();
+        });
+    });
+  });
+
+  // TEST UPDATE
+  it("should update a SINGLE pet on /pets/<id> PUT", (done) => {
+    var pet = new Pet(fido);
+    pet.save((err, data) => {
+      chai
+        .request(server)
+        .put(`/pets/${data._id}?_method=PUT`)
+        .send({ name: "Spider" })
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.html;
+          done();
+        });
+    });
+  });
+  
+  // SEARCH
+  it("should search ALL pets by name on /search GET", (done) => {
+    chai
+      .request(server)
+      .get("/search?term=norman")
       .end((err, res) => {
         res.should.have.status(200);
-        res.should.be.html
+        res.should.be.html;
         done();
       });
+  });
+
+  // TEST DELETE
+  it("should delete a SINGLE pet on /pets/<id> DELETE", (done) => {
+    var pet = new Pet(fido);
+    pet.save((err, data) => {
+      chai
+        .request(server)
+        .delete(`/pets/${data._id}?_method=DELETE`)
+        .end((err, res) => {
+          res.should.have.status(200);
+          res.should.be.html;
+          done();
+        });
     });
   });
 });
