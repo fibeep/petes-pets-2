4d1ea3e9b1750f8adfe991671cd85a837825dd09
Salo <salomonco@gmail.com>
2022-05-27 17:55:53 -0500

    added pagination plugin

--- routes/index.js
+++ routes/index.js
@@ -1,11 +1,10 @@
 const Pet = require('../models/pet');
 
 module.exports = (app) => {
-
   /* GET home page. */
-  app.get('/', (req, res) => {
-    Pet.find().exec((err, pets) => {
-      res.render('pets-index', { pets: pets });    
+  app.get("/", (req, res) => {
+    Pet.paginate().then((results) => {
+      res.render("pets-index", { pets: results.docs });
     });
   });
 }
