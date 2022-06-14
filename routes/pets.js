e3633edaa838cb3877b942d2f5768e198f1d17f7
Salo <salomonco@gmail.com>
2022-06-13 21:06:11 -0500

    implemented stripe payment

--- routes/pets.js
+++ routes/pets.js
@@ -120,8 +120,8 @@ module.exports = (app) => {
     });
   });
 
-  // PURCHASE
-  app.post("/pets/:id/purchase", (req, res) => {
+// PURCHASE
+  app.post('/pets/:id/purchase', (req, res) => {
     console.log(req.body);
     // Set your secret key: remember to change this to your live secret key in production
     // See your keys here: https://dashboard.stripe.com/account/apikeys
@@ -131,16 +131,25 @@ module.exports = (app) => {
     // Get the payment token ID submitted by the form:
     const token = req.body.stripeToken; // Using Express
 
-    const charge = stripe.charges
-      .create({
-        amount: 999,
-        currency: "usd",
-        description: "Example charge",
-        source: token,
-      })
-      .then(() => {
+    // req.body.petId can become null through seeding,
+    // this way we'll insure we use a non-null value
+    let petId = req.body.petId || req.params.id;
+
+    Pet.findById(petId).exec((err, pet)=> {
+      if (err) {
+        console.log('Error: ' + err);
         res.redirect(`/pets/${req.params.id}`);
+      }
+      const charge = stripe.charges.create({
+        amount: pet.price * 100,
+        currency: 'usd',
+        description: `Purchased ${pet.name}, ${pet.species}`,
+        source: token,
+      }).then((chg) => {
+        res.redirect(`/pets/${req.params.id}`);
+      })
+      .catch(err => {
+        console.log('Error:' + err);
       });
-  });
-  
-}
+    })
+})}
