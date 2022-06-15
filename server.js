179211c24d17f56e0039cb8b0f84ce0fa85c3a9b
Salo <salomonco@gmail.com>
2022-06-15 17:58:05 -0500

    started email sending but it crashes

--- server.js
+++ server.js
@@ -21,6 +21,43 @@ mongoose.connect('mongodb://localhost/local', {
   useFindAndModify: false
 });
 
+// require our mailgun dependencies
+const nodemailer = require('nodemailer');
+const mg = require('nodemailer-mailgun-transport');
+
+// auth with our mailgun API key and domain
+const auth = {
+  auth: {
+    api_key: process.env.MAILGUN_API_KEY,
+    domain: process.env.EMAIL_DOMAIN,
+  },
+};
+
+// create a mailer
+const nodemailerMailgun = nodemailer.createTransport(mg(auth));
+// SEND EMAIL
+const user = {
+  email: 'mg.salo.com',
+  name: 'Emily',
+  age: '43'
+};
+
+// nodemailerMailgun.sendMail({
+//   from: 'no-reply@example.com',
+//   to: user.email, // An array if you have multiple recipients.
+//   subject: 'Hey you, awesome!',
+//   template: {
+//     name: 'email.handlebars',
+//     engine: 'handlebars',
+//     context: user
+//   }
+// }).then(info => {
+//   console.log('Response: ' + info);
+// }).catch(err => {
+//   console.log('Error: ' + err);
+// });
+
+
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
